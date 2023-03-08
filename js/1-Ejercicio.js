159dbc36158e276af2b29bfe8eae2520276bc45c
Juan Barrionuevo <jottabarrionuevo@gmail.com>
2023-03-08 12:39:21 -0300

    se completo el ejercicio 2

--- js/1-Ejercicio.js
+++ js/1-Ejercicio.js
@@ -1,10 +1,12 @@
 let edad = parseInt(prompt("Ingrese su edad"));
 if(!isNaN(edad) === true ){
     
-if(edad >= 18)
+if(edad >= 18 )
     document.write("Ya puede conducir");
 }else{
-    document.write(`Ingresaste un texto en lugar de un numero`);    
-
+    document.write(`La edad ingresada no es un número válido`);    
+   }
+if(edad <= 17){
+    document.write("No puede conducir");
 
 }
